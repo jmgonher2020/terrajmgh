d15beaf2aadc09115ae5b2519ee9658bbbd0e64b
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-13 15:33:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "pod" {
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaazcus7tl3fravsyw2p7w6dg5biixbwnpx3f
 g5cqikz3h2w6hwfiqq"
   private_key_path = "/home/ubuntu/.oci/oci_api_key.pem"
+  private_key_password = "infranet2k55"
   compartment_ocid = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok7
 46k7r4waw3ujfgl4eh2sv73ipa"
   user_ocid = "ocid1.user.oc1..aaaaaaaa5h4fyfbcmu3jcyd2is4734clobpkcrzfebjuw5f2
