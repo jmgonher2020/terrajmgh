a41583e2c5b97203b44a410ed12cc5dae19ddb67
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-13 20:33:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 #module "pod" {
-resource "pod" {  
+#resource "pod" {  
 #  source = "./modulos/"
 #  source = "github.com/jmgonher2020/terrajmgh"
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaazcus7tl3fravsyw2p7w6dg5biixbwnpx3fg5cqikz3h2w6hwfiqq"
-  private_key_path = "/home/ubuntu/.oci/oci_api_key.pem"
-  private_key_password = "infranet2k55"
-  compartment_ocid = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
-  user_ocid = "ocid1.user.oc1..aaaaaaaa5h4fyfbcmu3jcyd2is4734clobpkcrzfebjuw5f2hknftkav5lea"
-  fingerprint = "ff:69:af:e8:8d:c6:83:ba:54:fb:67:2d:09:9b:00:41"
-  region = "us-ashburn-1"
-}
+#  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaazcus7tl3fravsyw2p7w6dg5biixbwnpx3fg5cqikz3h2w6hwfiqq"
+#  private_key_path = "/home/ubuntu/.oci/oci_api_key.pem"
+#  private_key_password = "infranet2k55"
+#  compartment_ocid = "ocid1.compartment.oc1..aaaaaaaauow2wnn5drbtbuaak7s2h54ok746k7r4waw3ujfgl4eh2sv73ipa"
+#  user_ocid = "ocid1.user.oc1..aaaaaaaa5h4fyfbcmu3jcyd2is4734clobpkcrzfebjuw5f2hknftkav5lea"
+#  fingerprint = "ff:69:af:e8:8d:c6:83:ba:54:fb:67:2d:09:9b:00:41"
+#  region = "us-ashburn-1"
+#}
 output "lb_ip" {
 #  value = "${pod.kubernetes_service.echo.load_balancer_ingress.0.ip}"
   value = "${module.pod.lb_ip}"
