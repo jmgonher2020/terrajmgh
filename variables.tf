80eb380c2c3443f19cce1b230938be6151f1fbf8
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-22 22:07:44 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "internet_gateway_enabled" {
 }
 
 variable "worker_ol_image_name" {
-  default = "Oracle-Linux-7.8-2020.06.09-0"
+  default = "Oracle-Linux-7.8"
   #default = "Oracle-Linux-7.5"
 }
 
