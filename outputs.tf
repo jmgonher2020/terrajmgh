007fbff74e789cfedc49e1daf7d903b7a7112418
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-27 17:34:05 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "lb_ip" {
   value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
 }
+resource sendmail_send email {
+  from = "jmgh@jmgh.com"
+  to = "jmgonzalez@aserta.mx"
+  subject = "A Terraform Email"
+  body = <<EMAIL
+Hello, this is an email from terraform.
+EMAIL
+}
