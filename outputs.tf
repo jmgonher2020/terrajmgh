852060cdec0e63c8f4688ee3e37d1a9011233e95
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-27 19:07:04 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "lb_ip" {
-  value = "${kubernetes_service.echo.load_balancer_ingress.0.host}"
+  value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
 }
 output "replicas" {
   value = "${kubernetes_deployment.echo.spec.0.replicas}"
