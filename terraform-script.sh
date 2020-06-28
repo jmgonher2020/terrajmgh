6147efc1e8a5450d61a654093cafd4c92e14b121
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-28 00:41:11 -0500

    Create terraform-script.sh

---
+++ terraform-script.sh
@@ -0,0 +1,6 @@
+/usr/local/bin/terraform init
+/usr/local/bin/terraform plan
+/usr/local/bin/terraform get -update
+#/usr/local/bin/terraform apply -auto-approve
+/usr/local/bin/terraform destroy -auto-approve
+#/usr/local/bin/terraform output
