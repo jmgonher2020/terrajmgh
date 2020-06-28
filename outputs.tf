671fb354fd1e5c616c580519d57af5b94c3bd349
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-27 19:52:28 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "lb_ip" {
+output "Servicio_ip" {
   value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
 }
-output "replicas" {
+output "Replicas" {
   value = "${kubernetes_deployment.echo.spec.0.replicas}"
 }
-output "nodos" {
+output "Nodos" {
   value = "${oci_containerengine_node_pool.k8s_node_pool.node_config_details.0.size}"
 }
 output "K8s_Version" {
