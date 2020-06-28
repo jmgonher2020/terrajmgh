7f2ae123bd8cabdc8e101a47025e33691b357acb
jmgonher2020 <62194140+jmgonher2020@users.noreply.github.com>
2020-06-27 23:51:32 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "kubernetes" {
-  config_path = "/var/lib/jenkins/.kube/config"
+#  config_path = "/var/lib/jenkins/.kube/config"
+  config_path = "${data.container_cluster_config.test_cluster_config.config_file_path}"
   config_context_auth_info = "target-cluster"
 }
